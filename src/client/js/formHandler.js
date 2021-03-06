59427e2d60ef050a650b83dc34c068651a63fd2c
omar-etman <omartareketman@gmail.com>
2021-03-06 22:50:36 +0200

    Update formHandler.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,10 +2,11 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').
-    
+    let formText = document.getElementById('name').value
+
     Client.checkForName(formText)
 
+    console.log("::: Form Submitted :::")
     fetch('http://localhost:8081/test')
     .then(res => {
         return res.json()
