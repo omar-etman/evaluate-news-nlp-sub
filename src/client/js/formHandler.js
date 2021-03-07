dc5dff520482eb5843a1af65228c5f8534c45a1a
omar-etman <omartareketman@gmail.com>
2021-03-07 14:51:09 +0200

    adjust server - api

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,19 +1,26 @@
-function handleSubmit(event) {
+async function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
 
     Client.checkForName(formText)
-
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => {
-        return res.json()
-    })
-    .then(function(data) {
-        document.getElementById('results').innerHTML = data.message
-    })
+
+    const res = await fetch('http://localhost:8081/addData', {
+        method: 'POST', 
+        credentials: 'same-origin', 
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({formText}), 
+      });
+      try {
+        const data = await res.json();
+        document.getElementById('results').innerHTML = data.message;
+    }catch(error) {
+        console.log('error',error);
+    }
 }
 
 export { handleSubmit }
--- src/server/const dotenv = require('dotenv');.js
+++ src/server/const dotenv = require('dotenv');.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
@@ -8,33 +11,36 @@ let baseURL = 'https://api.meaningcloud.com/sentiment-2.1?key=';
 const json = '&of=json&txt=';
 
 const end = '&model=General&lang=en';
-const fetch = require("node-fetch");
+
+
 
 const app = express()
-app.use(cors())
-// to use json
-app.use(bodyParser.json())
-// to use url encoded values
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
 
 app.use(express.static('dist'))
+app.use(cors);
+app.use(bodyParser);
+console.log(__dirname)
+
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
 })
 
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('dist/index.html'));
+})
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
+
 const apiKey = process.env.API_KEY
 console.log(`Your API key is ${process.env.API_KEY}`);
-console.log(JSON.stringify(mockAPIResponse))
 
 app.post('/addData', async(req, res)=>{
-    console.log('==============')
-    console.log('BODY', req.body);
-    const text = req.body.formText || '';
-    const getSentiment = await fetch(`${baseURL}${apiKey}${json}${text}${end}`,{
+    const getSentiment = await fetch(`${baseURL}${apiKey}${json}${req.body.formText}${end}`,{
         method: 'POST'
 });
     try{
@@ -45,4 +51,3 @@ app.post('/addData', async(req, res)=>{
         console.log("error", error);
     }
 });
-
